ad949f68a7bef839877a578e5f5cc728c7ed59d6
Vivian <vivian.phung17@gmail.com>
2019-01-07 10:25:41 -0500

    Updated the main program and the animation panel
    Set up the frame and the panel for the game.

--- stamonopoly.java
+++ stamonopoly.java
@@ -7,11 +7,30 @@ import java.io.*;
 
 public class stamonopoly{
 	//Properties
+	JFrame frame = new JFrame("STA MONOPOLY");
+	JPanel panel = new AnimationMonopolyPanel();
+	Timer timer;
 	
 	//Methods
 	
 	//Constructor
-	
+	public stamonopoly(){
+		//Panel 
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(1280,720));
+		
+		//Frame
+		frame.setContentPane(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false); //prevents windows from being resized
+		
+		
+	}
 	//Main Method
+	public static void main (String[]  args){
+		new stamonopoly();
+	}
 	
 }
