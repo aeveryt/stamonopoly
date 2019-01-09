0bda358c5d58176009717c3569e56485c2410a8d
aevery <aeverytam@gmail.com>
2019-01-09 09:50:02 -0500

    Trying to add button on instructions screen

--- mainmenu.java
+++ mainmenu.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*; 
 import java.awt.*; 
 import java.awt.event.*; 
@@ -13,13 +12,22 @@ public class mainmenu implements ActionListener{
 	public JButton ExitButt; 
 	public JButton PlayButt;
 	public monopolyinstructions thepanelinstructions;
+	public JButton backtomain;
 
 	
 	//Methods
 	public void actionPerformed(ActionEvent evt){
 		if(evt.getSource() == InstrucButt){
+			
+			
 			theframe.setContentPane(thepanelinstructions);
+			backtomain.setVisible(true);
+			backtomain.repaint();
 			theframe.setVisible(true);
+			
+			//if(evt.getSource() == backtomain){
+				//System.out.println("button is clicked");
+			//}
 		
 		}else if(evt.getSource() == ExitButt){
 			System.exit(0);
@@ -32,51 +40,58 @@ public class mainmenu implements ActionListener{
 	//Constructor
 	public mainmenu(){
 	
+		//set variables
 		theframe = new JFrame("Main Menu");
 		thepanel = new JPanel(); 
+		thepanelinstructions = new monopolyinstructions();
+		
+		//set panel
 		thepanel.setPreferredSize(new Dimension(1280,720)); 
 		thepanel.setLayout(null); 
 		
-		thepanelinstructions = new monopolyinstructions();
-		
-		// instruction button 
-		
+		//instruction button 
 		InstrucButt = new JButton("Instructions"); 
 		InstrucButt.setSize(120, 30); 
 		InstrucButt.setLocation(110,250); 
-		InstrucButt.addActionListener(this); 
-		
-		// play button 
-
-	// 	PlayButt.setFont(new Font("kabel.ttf", Font.PLAIN, 40));
+		InstrucButt.addActionListener(this);
+	
+		//play button 
+		//PlayButt.setFont(new Font("kabel.ttf", Font.PLAIN, 40));
 		PlayButt = new JButton("Play"); 
 		PlayButt.setSize(200, 35); 
 		PlayButt.setLocation(75,200); 
 		PlayButt.addActionListener(this); 
 		
-		// exit button 
+		//exit button 
 		ExitButt = new JButton("Exit"); 
 		ExitButt.setSize(80, 30); 
 		ExitButt. setLocation(1200, 0); 
 		ExitButt.addActionListener(this);
 		
-		//Insert image
+		//insert image
 		ImageIcon icon = new ImageIcon("Homepage.jpg"); 
 		JLabel label = new JLabel(icon);
 		label.setSize(1280,720);  
 		label.setLocation(0,0); 
 		
+		//INSTRUCTIONS SCREEN
+		//- back to main menu button
+		backtomain = new JButton("BACK TO MAIN MENU");
+		backtomain.setSize(150,35);
+		backtomain.setLocation(20,20);
+		backtomain.addActionListener(this);
+		backtomain.setVisible(false);
 		
-		//Add Images
+		//add features
 		thepanel.add(InstrucButt);
 		thepanel.add(PlayButt); 
-		thepanel.add(ExitButt); 
+		thepanel.add(ExitButt);
+		thepanel.add(backtomain); 
 		thepanel.add(label); 
  
-		
+		//set frame
 		theframe.setContentPane(thepanel);
 		theframe.pack();
-		
 		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		theframe.setVisible(true);
 	
--- monopolyinstructions.java
+++ monopolyinstructions.java
@@ -9,6 +9,8 @@ public class monopolyinstructions extends JPanel{
 	//Properties
 	Font title;
 	BufferedImage page1;
+	public JButton backtomain;
+	//public JPanel panel;
 	
 	//-initialize
 
@@ -26,6 +28,18 @@ public class monopolyinstructions extends JPanel{
 		//from JPanel
 		super();
 		
+		//set panel properties
+		//panel = new JPanel();
+		//panel.setPreferredSize(new Dimension(1280,720));
+		//panel.setLayout(null);
+		//- back to main menu button
+		backtomain = new JButton("BACK TO MAIN MENU");
+		backtomain.setSize(150,35);
+		backtomain.setLocation(20,20);
+		//backtomain.addActionListener(this);
+		//backtomain.setVisible(false);
+		
+		//panel.add(backtomain);
 		//checking whether or not image or font is available
 		try{
 			page1 = ImageIO.read(new File("instructions.jpg"));
