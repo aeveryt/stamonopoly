0ceae4b87f471e6e425f418417bd78728a3d5eb3
Vivian <vivian.phung17@gmail.com>
2019-01-16 21:51:36 -0500

    Added Community and Chance csv files
    loaded community csv file onto file

--- stamonopolyproperties.java
+++ stamonopolyproperties.java
@@ -1,9 +1,4 @@
 //Properties File 
-// Change Arc to one used in Exception....
-
-//Fix CSV File 
-
-import arc.*;
 import java.io.*;
 import java.awt.*;
 import java.awt.image.*;
@@ -17,31 +12,41 @@ public class stamonopolyproperties{
 	boolean Geo = false;
 	boolean blnFileFail = false;
 	FileReader thefile = null;
+	FileReader chancefile = null;
+	FileReader communityfile = null;
 	BufferedReader properties = null;
+	BufferedReader community = null;
+	BufferedReader chance = null;
 	
 	//Split Data
 	String strSplit[];
-	String strProperties[][] = new String[31][9];
-		
+	String strProperties[][] = new String[31][10];
+	String strCommunity [][] = new String[30][3];
+	String strChance [][] = new String[30][3];
 	
 	//Methods
 	
 
-		//reading the file 
+		//reading the Properties file
 		try{
 			thefile = new FileReader("properties.csv");
+			communityfile = new FileReader("community.csv");
+			//chancefile = new FileReader("chance.csv");
 		}catch(FileNotFoundException e){
 			System.out.println("Unable to read File");
 			blnFileFail = true;
 		}
-		
-		
+	
 		properties = new BufferedReader(thefile);
+		community = new BufferedReader(communityfile);
+		
 		
 		String strLine = "";
 		int intRow;
 		int intCol;
 		
+		
+		//Properties File
 			for (intRow = 0; intRow < 31; intRow ++){
 				try{	
 					strLine = properties.readLine();
@@ -49,12 +54,37 @@ public class stamonopolyproperties{
 					System.out.println("false");
 			}
 				strSplit = strLine.split(",");
-			for (intCol = 0; intCol < 9; intCol++){
-			
-				strProperties[intRow][intCol] = strSplit[intCol];
-			
+			for (intCol = 0; intCol < 10; intCol++){	
+				strProperties[intRow][intCol] = strSplit[intCol];				
 				System.out.println(strProperties[intRow][intCol]);
 			}
 		}
+		//Community Files
+		//Organized as Card Number, Statement, Money Given or Owed
+		
+		for (intRow = 0; intRow < 30; intRow ++){
+				try{
+					strLine = community.readLine();
+				}catch(IOException e){
+					System.out.println("false");
+			}
+				strSplit = strLine.split(",");
+			for (intCol = 0; intCol < 3; intCol++){
+				
+				strCommunity[intRow][intCol] = strSplit[intCol];
+				System.out.println(strCommunity[intRow][intCol]);
+			}
+		}
+		
+		//Closing file
+		
+		try{
+			thefile.close();
+			communityfile.close();
+		
+		}catch(IOException e){
+			System.out.println("CANNOT CLOSE FILE");
+		}
+			
 	}
 }
